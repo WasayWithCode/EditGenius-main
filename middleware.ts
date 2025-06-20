21f2b8b142d4b4aec1a20c988868abe69bf25ce1
Abdul Wasay <abdulwasay43429@gmail.com>
2025-06-20 18:08:18 +0500

    add websocket support to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/api/webhooks/clerk',
+  // Add other public routes here
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect(); // Protect all routes except public ones
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}; 
+};
